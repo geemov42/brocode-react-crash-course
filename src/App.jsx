afb0680ff420f30395454192a54c0c480e15206d
geemov42 <geemov42@gmail.com>
2024-05-15 21:44:49 +0200

    structured props with react, type validation, default values

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   return (
     <>
-      <Button />
+      <Student name="Kev" age={39} isStudent={true} />
+      <Student name="Marie" age={37} isStudent={false} />
+      <Student name="Quentin" age={'10'} isStudent={true} />
+      <Student name="Céleste" age={8} isStudent={true} />
+      <Student age={8} isStudent={true} />
     </>
   );
 }
