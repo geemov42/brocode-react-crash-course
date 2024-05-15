ad84c9581137228a9175291c573fa9e6b23625c6
geemov42 <geemov42@gmail.com>
2024-05-15 22:27:37 +0200

    working with object list

--- src/List.jsx
+++ src/List.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const List = () => {
+  const fruits = [
+    { name: "Apple", calories: 95 },
+    { name: "Orange", calories: 45 },
+    { name: "Banana", calories: 105 },
+    { name: "Pineapple", calories: 37 },
+  ];
 
-    const fruits = ['Apple', 'Orange', 'Banana', 'Pineapple'];
-    fruits.sort();
+//   fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical order
+  fruits.sort((a, b) => a.calories - b.calories); // Numeric order
 
   return (
     <ul>
-        {fruits.map((element, idx) => {
-            return <li key={idx}>{element}</li>;
-        })}
+      {fruits.map((element, idx) => {
+        return <li key={idx}>{element.name} : &nbsp; <b>{element.calories}</b></li>;
+      })}
     </ul>
-  )
-}
+  );
+};
 
 export default List;
