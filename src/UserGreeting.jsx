62889fed680ea96af50fb283d4b59ae118e12dc1
geemov42 <geemov42@gmail.com>
2024-05-15 22:09:10 +0200

    conditional rendering with ternary inlined and proptype validation

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import propTypes from 'prop-types';
 
 const UserGreeting = (props) => {
-  return props.isLoggedIn ? (
-    <h2 className="welcome-message">Welcome {props.username}</h2>
-  ) : (
-    <h2 className="login-prompt">Please log in to continue</h2>
-  );
+    
+  const welcomeMessage = <h2 className="welcome-message">Welcome {props.username}</h2>;
+  const loginPrompt = <h2 className="login-prompt">Please log in to continue</h2>;
+
+  return props.isLoggedIn ? welcomeMessage : loginPrompt;
 };
+
+UserGreeting.propTypes = {
+    isLoggedIn: propTypes.bool,
+    username: propTypes.string
+};
+
+UserGreeting.defaultProps = {
+    isLoggedIn: false,
+    username: 'Guest'
+}
 
 export default UserGreeting;
