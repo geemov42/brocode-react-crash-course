9099bff0f7de93e34e134ab1b26e122ccfff09d2
geemov42 <geemov42@gmail.com>
2024-05-16 14:59:07 +0200

    useState with form element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import MyComponent from "./MyComponent";
-import Counter from "./Counter";
 
 function App() {
   return (
     <>
-      <Counter />
-      <MyComponent />
+    <MyComponent />
     </>
   );
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,32 +1,57 @@
 import React, { useState } from "react";
 
 const MyComponent = () => {
-  const [name, setName] = useState(null);
-  const [age, setAge] = useState(0);
-  const [isEmployed, setIsEmployed] = useState(false);
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [comment, setComment] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const [shippingMethod, setShippingMethod] = useState('');
 
-  const updateName = () => {
-    setName("Sponge Bob");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
   };
 
-  const incrementAge = () => {
-    setAge(age + 1);
+  const handleQuantityChange = (e) => {
+    setQuantity(e.target.value);
   };
 
-  const toggleEmployed = () => {
-    setIsEmployed(!isEmployed);
-  };
+  function handlePaymentMethodChange(e) {
+    setPaymentMethod(e.target.value);
+  }
+
+  function handleShippingMethodChange(e) {
+    setShippingMethod(e.target.value);
+  }
 
   return (
     <div>
-      <p>Name: {name}</p>
-      <button onClick={updateName}>set name</button>
+      <input value={name} onChange={handleNameChange} />
+      <p>Name : {name}</p>
 
-      <p>Age: {age}</p>
-      <button onClick={incrementAge}>increment age</button>
+      <input type="number" value={quantity} onChange={handleQuantityChange} />
+      <p>Quantity : {quantity}</p>
 
-      <p>Is employed: {isEmployed ? 'Yes' : 'No'}</p>
-      <button onClick={toggleEmployed}>Toggle employed</button>
+      <textarea value={comment} onChange={(e) => setComment(e.target.value)} />
+      <p>Comment : {comment}</p>
+
+      <select value={paymentMethod} onChange={handlePaymentMethodChange}>
+        <option value="">Select an option</option>
+        <option value="VISA">Visa</option>
+        <option value="MASTERCARD">mastercard</option>
+      </select>
+      <p>Payment method : {paymentMethod}</p>
+
+      <label>
+        <input type="radio" value="Pick up" checked={shippingMethod === "Pick up"} onChange={handleShippingMethodChange} />
+        Pick up
+      </label>
+
+      <label>
+        <input type="radio" value="Delivery" checked={shippingMethod === "Delivery"} onChange={handleShippingMethodChange} />
+        Delivery
+      </label>
+      
+      <p>Shipping method : {shippingMethod}</p>
     </div>
   );
 };
