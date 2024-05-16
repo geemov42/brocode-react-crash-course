c0379ce71c5d4c268bf801f48c7842d01d14ce69
geemov42 <geemov42@gmail.com>
2024-05-16 16:42:44 +0200

    colorPicker app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MyComponent from "./MyComponent";
+import ColorPicker from "./ColorPicker";
 
 function App() {
   return (
     <>
-    <MyComponent />
+      <ColorPicker />
     </>
   );
 }
