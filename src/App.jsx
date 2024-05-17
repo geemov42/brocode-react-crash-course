aeba335db02418b7f945f35fb594905cd523bf71
geemov42 <geemov42@gmail.com>
2024-05-17 12:19:15 +0200

    Digital clock - useState useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MyComponent from "./MyComponent";
+import DigitalClock from "./DigitalClock";
 
 function App() {
   return (
     <>
-    <MyComponent />
+    <DigitalClock />
     </>
   );
 }
